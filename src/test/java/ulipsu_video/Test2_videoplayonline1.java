5b55ef258a61553d890520067933f46b17cb6a23
ManojSelvam1998 <121037158+ManojSelvam1998@users.noreply.github.com>
2024-11-08 22:31:00 +0530

    Add files via upload

---
+++ src/test/java/ulipsu_video/Test2_videoplayonline1.java
@@ -0,0 +1,62 @@
+package ulipsu_video;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileInputStream;
+import java.io.IOException;
+public class Test2_videoplayonline1 {		//VIDEO
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        List<String> urls = readUrlsFromExcel("C:\\Users\\manoj\\Downloads\\TEST_VIDEO.xlsx");
+        int batchSize = 2;
+        int playTimeInSeconds = 10;
+
+        for (int i = 0; i < urls.size(); i += batchSize) {
+            List<String> batch = urls.subList(i, Math.min(i + batchSize, urls.size()));
+            List<WebDriver> drivers = new ArrayList<>();
+            for (String url : batch) {
+                WebDriver driver = new ChromeDriver();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                driver.manage().window().maximize();
+                drivers.add(driver);
+                driver.get(url);
+            }
+            
+            Thread.sleep(playTimeInSeconds * 1000);
+            for (WebDriver driver : drivers) {
+                driver.quit();
+            }
+
+            Thread.sleep(1000);
+        }
+    }
+
+    public static List<String> readUrlsFromExcel(String filePath) {
+        List<String> urls = new ArrayList<>();
+
+        try (FileInputStream file = new FileInputStream(filePath);
+             Workbook workbook = new XSSFWorkbook(file)) {
+
+            Sheet sheet = workbook.getSheetAt(0);  
+
+            for (Row row : sheet) {
+                String url = row.getCell(0).getStringCellValue(); 
+                urls.add(url);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return urls;
+    }
+}
